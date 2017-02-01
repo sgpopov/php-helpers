d1eb2c67b108ba8d6e804794a052690228191b86
Svilen Popov <svil4ok@gmail.com>
2017-02-01 10:06:03 +0200

    New: URL friendly slug

--- src/Str.php
+++ src/Str.php
@@ -22,4 +22,43 @@ class Str
 
         return false;
     }
+
+    /**
+     * Returns URL friendly slug version of input string:
+     *  - converts all alpha chars to lowercase
+     *  - converts any char that is not digit, letter or - into - symbols into "-"
+     *  - not allow two "-" chars continued, converte them into only one syngle "-"
+     *
+     * For example: 'This iS a sImpLe TEST' => 'this-is-a-simple-tests'
+     *
+     * @param string $input
+     * @param string $separator
+     *
+     * @return string
+     */
+    public static function slug(string $input, $separator = '-') : string
+    {
+        // Make sure the string is UTF-8 encoded.
+        if ($input !== mb_convert_encoding(mb_convert_encoding($input, 'UTF-32', 'UTF-8'), 'UTF-8', 'UTF-32')) {
+            $input = mb_convert_encoding($input, 'UTF-8', mb_detect_encoding($input));
+        }
+
+        // Convert all applicable characters to HTML entities.
+        $input = htmlentities($input, ENT_NOQUOTES, 'UTF-8');
+
+        // Converts all special characters to their base form (i.e. ü -> u).
+        $input = preg_replace('`&([a-z]{1,2})(acute|uml|circ|grave|ring|cedil|slash|tilde|caron|lig);`i', '\1', $input);
+
+        // Convert all HTML entities to their applicable characters.
+        $input = html_entity_decode($input, ENT_NOQUOTES, 'UTF-8');
+
+        // Convert all spaces to hyphens (or whatever the separator) as well as
+        // repating hyphens (or whatever the separator) will be reduce to just one.
+        $input = preg_replace(['`[^a-z0-9]`i', '`['. $separator .']+`'], $separator, $input);
+
+        // Trim the string and convert it to lowercase.
+        $input = strtolower(trim($input, $separator));
+
+        return $input;
+    }
 }
--- tests/StrTest.php
+++ tests/StrTest.php
@@ -26,4 +26,29 @@ class StrTest extends PHPUnit_Framework_TestCase
             'not exists'
         ]));
     }
+
+    /**
+     * @test
+     *
+     * @covers Str::slug()
+     */
+    public function shouldCreateUrlSlug()
+    {
+        $tests = [
+            'this is easy' => 'this-is-easy',
+            'tHiS        iS eASY' => 'this-is-easy',
+            'this--------is easy' => 'this-is-easy',
+            '<img src="image.png" />' => 'img-src-image-png',
+            'ä ö ü ß €' => 'a-o-u-sz', // the euro sign is ignored
+            'TeRríbLé(!) STRinG' => 'terrible-string',
+            '!@~$%#^&()_+}{.;\'[]}' => '',
+            '$ ₡ ₱ £ ¢ £ ₨' => '',
+        ];
+
+        foreach ($tests as $input => $expected) {
+            $this->assertSame($expected, Str::slug($input));
+        }
+
+        $this->assertSame('this.is.easy', Str::slug('this..........is easy', '.'));
+    }
 }
